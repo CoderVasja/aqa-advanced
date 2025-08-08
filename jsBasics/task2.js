6d835361aab346397f925507db6503aadc970657
Vasja <makovasja@gmail.com>
2025-08-08 21:40:53 +0300

    homework_4.1 done

---
+++ jsBasics/task2.js
@@ -0,0 +1,9 @@
+let person1 = 'Vasja';
+let person2 = 'Pasha';
+let greeting = "Hello, " + person1 + " та " + person2 + "!";
+
+console.log(greeting);
+
+let greetingTemplate = `Hello, ${person1} and ${person2}`;
+
+console.log(greetingTemplate)
