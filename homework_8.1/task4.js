299bfbbf1eb61539ea1821a8d4d53c569980b93b
Vasja <makovasja@gmail.com>
2025-09-09 20:31:43 +0300

    homework_12.1 done

--- homework_8.1/task4.js
+++ homework_8.1/task4.js
@@ -1,11 +1,14 @@
-const findEvenNumbers = (numbers) =>{
+const findEvenNumbers = (numbers) => {
     let evenNumbers = [];
-
-    for(let i = 0; i < numbers.length; i++){
-        numbers[i] % 2 == 0 ? evenNumbers.push(numbers[i]) : 0;
+  
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i] % 2 === 0) {
+        evenNumbers.push(numbers[i]);
+      }
     }
-    
-    console.log(evenNumbers)
-}
-
-findEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+  
+    console.log(evenNumbers);
+  };
+  
+  findEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  
